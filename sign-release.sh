39aed428d66863a4c923e5b15fcb7f885a524dee
Anish Nayak <anish1.nayak@gmail.com>
2025-07-18 17:12:12 +0530

    dv

--- sign-release.sh
+++ sign-release.sh
@@ -9,3 +9,4 @@ apt-ftparchive release $DIST_DIR > $DIST_DIR/Release
 # Sign with your GPG key
 gpg --default-key "Secmia Apt Signing" -abs -o $DIST_DIR/Release.gpg $DIST_DIR/Release
 gpg --default-key "Secmia Apt Signing" --clearsign -o $DIST_DIR/InRelease $DIST_DIR/Release
+echo "Release file and signatures created in $DIST_DIR"
